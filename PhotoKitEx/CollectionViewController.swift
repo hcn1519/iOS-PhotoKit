4b3740f726e0c595a81504874442c56152fa900c
Hong, Chang Nam <hcn1519@gmail.com>
2017-02-15 17:47:33 +0900

    기기에 있는 사진 로딩

--- PhotoKitEx/CollectionViewController.swift
+++ PhotoKitEx/CollectionViewController.swift
@@ -7,18 +7,42 @@
 //
 
 import UIKit
+import Photos
 
 class CollectionViewController: UICollectionViewController {
+    
+    var assetsFetchResults: PHFetchResult<PHAsset> = PHFetchResult<PHAsset>()
+    var imageManger: PHCachingImageManager!
+    
+    override func viewDidLoad() {
+        PHPhotoLibrary.requestAuthorization() {(status) in switch status {
+        case .authorized:
+            self.imageManger = PHCachingImageManager()
+            let options = PHFetchOptions()
+            options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+            self.assetsFetchResults = PHAsset.fetchAssets(with: options)
+            self.collectionView?.reloadData()
+        default:
+            NSLog("사진에 접근할 수 없습니다.")
+            }
+            
+        }
+    }
+    
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 7
+        return self.assetsFetchResults.count
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! CollectionViewCell
-        let image = UIImage(named: "0\(indexPath.row + 1).jpeg")
-        cell.imageView.image = image
+        
+        let asset: PHAsset = self.assetsFetchResults[indexPath.item]
+        self.imageManger.requestImage(for: asset, targetSize: cell.frame.size, contentMode: PHImageContentMode.aspectFit, options: nil, resultHandler: { (result: UIImage?, info: [AnyHashable: Any]?) in
+            cell.imageView.image = result
+            })
         
         return cell
     }
